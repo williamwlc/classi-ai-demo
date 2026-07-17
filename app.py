7ad6246ccb4319fc3cafc67572881fa2eeeb859f
williamwlc <59538245+williamwlc@users.noreply.github.com>
2026-07-17 23:40:14 +0800

    Fix formatting of contact information section

--- app.py
+++ app.py
@@ -113,8 +113,7 @@ with col2:
 
 # Contact
 st.markdown("---")
-st.markdown("### Contact Us")
-st.markdown("📧 William@ClassiAIhk.com")
+st.markdown("### Contact Us") st.markdown("📧 William@ClassiAIhk.com")
 
 # Footer
 st.caption("© 2026 Classi AI. All rights reserved.")
